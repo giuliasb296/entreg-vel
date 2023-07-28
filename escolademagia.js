92faaecc59fce6c56e67963f1b959d244a763295
giuliasb296 <123772160+giuliasb296@users.noreply.github.com>
2023-07-28 16:09:13 -0300

    Add files via upload

---
+++ escolademagia.js
@@ -0,0 +1,20 @@
+let capacidade = "Transformação"
+let habilidade = "Controle de Elementos"
+let respeito = "10"
+let ética = "Sim"
+let equipe = "Sim"
+let aprender = "Sim"
+
+if (capacidade == "Transformação" && respeito >= 8) {
+    document.write("Você atende aos requisitos para se tornar um aprendiz na Academia")
+} 
+
+else if (capacidade == "Fogo" && habilidade == "Controle de Elementos" ) {
+    document.write("Você atende aos requisitos para se tornar um aprendiz na Academia");
+}
+
+else if (capacidade == "Ilusão" && ética == Sim && equipe == Sim) {
+    document.write("Você atende aos requisitos para se tornar um aprendiz na Academia");
+}
+
+else if ( )
